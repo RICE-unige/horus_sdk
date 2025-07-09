1ed3e56c5ee7678a0edebf4522d63e22a5a9c0b8
Omotoye Shamsudeen Adekoya <adekoyaomotoye@gmail.com>
2025-07-09 14:24:04 +0200

    🔧 Updated SDK imports after module reorganization
    Updated import statements to reflect new module structure

--- python/horus/__init__.py
+++ python/horus/__init__.py
@@ -2,5 +2,15 @@
 
 from .client import Client
 from .utils.branding import __version__
+from .robot import Robot, RobotType
+from .sensors import SensorType, Camera, LaserScan, Lidar3D
+from .dataviz import DataViz, VisualizationType, DataSourceType
+from .color import ColorManager, ColorScheme, RGBColor
 
-__all__ = ['Client', '__version__']
+__all__ = [
+    'Client', '__version__', 
+    'Robot', 'RobotType',
+    'SensorType', 'Camera', 'LaserScan', 'Lidar3D',
+    'DataViz', 'VisualizationType', 'DataSourceType',
+    'ColorManager', 'ColorScheme', 'RGBColor'
+]
---
+++ python/horus/core/__init__.py
@@ -0,0 +1,16 @@
+"""
+HORUS SDK Core Module
+
+Pure logic components that are ROS-agnostic and unit-testable.
+Contains low-level infrastructure like EventBus, TopicMap, and exceptions.
+"""
+
+# Import stub implementations for now
+from .event_bus import *
+from .topic_map import *
+from .exceptions import *
+
+__all__ = [
+    # Pure logic components (stubs for now, ready for implementation)
+    # 'EventBus', 'TopicMap', 'HorusException'
+]
